f4154c942aeb834d07342127c0ac77437be95738
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2025-04-16 12:08:44 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
- 
-  $(document).ready(function(){
+$(document).ready(function () {
     // Feature Carousel
-	$('.feature_carousel').owlCarousel({
-		items: 1,
-		loop: true,
-		nav: true,
-		dots: false,
-		// autoplay: true,
-		autoplayTimeout: 5000,  
-		autoplaySpeed: 1600,    
-		smartSpeed: 1600,      
-		margin: 20,
-		navText: [
-			'<span class="custom-nav prev"><i class="fa-solid fa-angle-left"></i></span>',
-			'<span class="custom-nav next"><i class="fa-solid fa-angle-right"></i></span>'
-		], 
-	});
-  }); 
+    $('.feature_carousel').owlCarousel({
+      items: 1,
+      loop: true,
+      nav: true,
+      dots: false,
+      autoplayTimeout: 5000,
+      autoplaySpeed: 1600,
+      smartSpeed: 1600,
+      margin: 20,
+      navText: [
+        '<span class="custom-nav prev"><i class="fa-solid fa-angle-left"></i></span>',
+        '<span class="custom-nav next"><i class="fa-solid fa-angle-right"></i></span>'
+      ],
+    });
+  
+    // Menu Toggle + Icon Switch
+    $('#menuToggleBtn').on('click', function () {
+      $('#mainMenu').slideToggle('');
+  
+      const $icon = $('#menuIcon');
+      if ($icon.hasClass('fa-bars-staggered')) {
+        $icon.removeClass('fa-bars-staggered').addClass('fa-xmark');
+      } else {
+        $icon.removeClass('fa-xmark').addClass('fa-bars-staggered');
+      }
+    });
+});
+  
